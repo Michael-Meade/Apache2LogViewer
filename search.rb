3f81840503421ae757ebc8cd5a7015b1090ea1d1
Michael-Meade <mameade@utica.edu>
2021-10-28 20:28:57 -0400

    search.rb did stuff. created a example of searching for an IP and getting certain eresults

---
+++ search.rb
@@ -0,0 +1,11 @@
+require_relative 'lib'
+require_relative 'gruff'
+
+j = {}
+Dir['*'].each do |file_name|
+    if file_name.include?("access.log")
+        json = Template.new(file_name).path_ip("117.131.199.100", 2)
+        j.merge!(json) { |k, m, n| m + n }
+    end
+end
+p j
